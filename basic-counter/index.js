13fcbf3e70e35dfac58fb0cde24f0dfbb7789baf
Derek Holland <77945662+derekholland@users.noreply.github.com>
2023-06-28 20:39:58 -0400

    Added all project files

---
+++ basic-counter/index.js
@@ -0,0 +1,12 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    countEl.textContent = 0
+    count = 0
+}
